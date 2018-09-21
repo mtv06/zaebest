f50ae3c7e734a0cd20bcfce81f70ad09bdee3f5a
amotov <motov.1984@mail.ru>
2018-09-21 17:24:51 +0300

    Добавлены все страницы

--- main/urls.py
+++ main/urls.py
@@ -1,2 +1,10 @@
 from django.conf.urls import url
 from . import views
+
+urlpatterns = [
+    url(r'^$', views.IndexListView.as_view(), name='index'),
+    url(r'^search/$', views.SearchListView.as_view(), name='search'),
+    url(r'^added-places/$', views.AddedPlacesListView.as_view(), name='added_places'),
+    url(r'^liked-places/$', views.LikedPlacesListView.as_view(), name='liked_places'),
+    url(r'^signup/$', views.SignupView.as_view(), name='signup'),
+]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from . import models
+from rest_framework import permissions
+from rest_framework.decorators import permission_classes
+from rest_framework.views import APIView
+from rest_framework.renderers import TemplateHTMLRenderer
+from rest_framework import response, status
 
-# Create your views here.
+
+@permission_classes((permissions.AllowAny,))
+class IndexListView(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = "zaebest/pages/index.html"
+
+    def get(self, request):
+        return response.Response(status=status.HTTP_200_OK)
+
+
+@permission_classes((permissions.AllowAny,))
+class SearchListView(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = "zaebest/pages/search.html"
+
+    def get(self, request):
+        return response.Response(status=status.HTTP_200_OK)
+
+
+@permission_classes((permissions.AllowAny,))
+class AddedPlacesListView(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = "zaebest/pages/added_places.html"
+
+    def get(self, request):
+        return response.Response(status=status.HTTP_200_OK)
+
+
+@permission_classes((permissions.AllowAny,))
+class LikedPlacesListView(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = "zaebest/pages/search.html"
+
+    def get(self, request):
+        return response.Response(status=status.HTTP_200_OK)
